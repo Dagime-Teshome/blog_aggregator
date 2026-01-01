412b949fa0e40a4eb2e2134c3442120b10882ed0
Dagime Teshome <dagimeteshome1@gmail.com>
2026-01-01 18:35:26 +0300

    export command types

--- internal/cli/command_types.go
+++ internal/cli/command_types.go
@@ -1,6 +1,9 @@
 package cli
 
-import "go/gator/internal/config"
+import (
+	"fmt"
+	"go/gator/internal/config"
+)
 
 type State struct {
 	Config *config.Config
@@ -15,10 +18,19 @@ type Commands struct {
 	ComMap map[string]func(*State, Command) error
 }
 
-func (c *Commands) register(name string, f func(*State, Command) error) {
-
+func (c *Commands) Register(name string, f func(*State, Command) error) {
+	// add the command and the handler function to the commands map
+	c.ComMap[name] = f
 }
 
-func (c *Commands) run(s *State, cmd Command) error {
+func (c *Commands) Run(s *State, cmd Command) error {
+	// run the command that is sent using the args from the command argument
+	value, ok := c.ComMap[cmd.Name]
+	if !ok {
+		return fmt.Errorf("Command doesn't exist")
+	}
+	if err := value(s, cmd); err != nil {
+		return err
+	}
 	return nil
 }
