e1e9755663d5bbaa8283137f6cb544fa3a0be425
Dagime Teshome <dagimeteshome1@gmail.com>
2026-01-01 18:35:47 +0300

    change command error strings

--- internal/cli/login_command.go
+++ internal/cli/login_command.go
@@ -2,9 +2,9 @@ package cli
 
 import "fmt"
 
-func handlerLogin(s *State, cmd Command) error {
+func HandlerLogin(s *State, cmd Command) error {
 	if len(cmd.Args) == 0 {
-		return fmt.Errorf("login command expects one argument but has received none")
+		return fmt.Errorf("User name Required")
 	}
 	err := s.Config.SetUser(cmd.Args[0])
 	if err != nil {
