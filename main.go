cbaa2f97eeabd57eedee01c9ee006b229f5f73eb
Dagime Teshome <dagimeteshome1@gmail.com>
2026-01-06 16:36:49 +0300

    update main code
    -update main to register and use new commands
    -open connection to database and add it to state struct

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"go/gator/internal/cli"
 	"go/gator/internal/config"
+	"go/gator/internal/database"
 	"log"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -19,16 +23,23 @@ func main() {
 		log.Fatal(fmt.Errorf("error reading config file:%w", err))
 		return
 	}
+	db, err := sql.Open("postgres", configStruct.Db_url)
+
+	db_queries := database.New(db)
+	state.Db = db_queries
 
 	state.Config = &configStruct
 	commands := cli.Commands{
 		ComMap: make(map[string]func(*cli.State, cli.Command) error, 0),
 	}
 	commands.Register("login", cli.HandlerLogin)
+	commands.Register("register", cli.Register)
+	commands.Register("reset", cli.Reset)
 	var command cli.Command
 	command.Name = os.Args[1]
 	command.Args = append(command.Args, os.Args[2:]...)
 	if err := commands.Run(&state, command); err != nil {
 		log.Fatal(err)
 	}
+
 }
