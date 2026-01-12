56a3afbd022e438b44d2eaca294076d0d2b0271b
Dagime Teshome <dagimeteshome1@gmail.com>
2026-01-12 14:58:06 +0300

    register aggregator command

--- main.go
+++ main.go
@@ -36,6 +36,7 @@ func main() {
 	commands.Register("register", cli.Register)
 	commands.Register("reset", cli.Reset)
 	commands.Register("users", cli.GetUsers)
+	commands.Register("agg", cli.Agg)
 	var command cli.Command
 	command.Name = os.Args[1]
 	command.Args = append(command.Args, os.Args[2:]...)
