2027448315e23e5851e5d51bfeb41ca3155389f5
Dagime Teshome <dagimeteshome1@gmail.com>
2026-01-15 20:09:28 +0300

    add feed query

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -10,9 +10,20 @@ VALUES(
 )
 RETURNING *;
 
+-- name: FollowFeed :one
+Insert into feed_follows (id,created_at,updated_at,user_id,feed_id)
+VALUES(
+    $1,
+    $2,
+    $3,
+    $4,
+    $5
+)
+RETURNING *,
+(select u.name from users u where user_id = users.id) As user_name,
+(select f.name from feeds f where feed_id = feeds.id) As feed_name;
 
 -- name: GetFeeds :many
-
 Select * from feeds;
 
 -- name: ResetFeeds :exec
