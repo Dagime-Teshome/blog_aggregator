a2922e8c0f5374974c96d8ac4cf77fe8bd691544
Dagime Teshome <dagimeteshome1@gmail.com>
2026-01-16 19:41:31 +0300

    refactor feeds query

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -10,18 +10,15 @@ VALUES(
 )
 RETURNING *;
 
--- name: FollowFeed :one
-Insert into feed_follows (id,created_at,updated_at,user_id,feed_id)
-VALUES(
-    $1,
-    $2,
-    $3,
-    $4,
-    $5
-)
-RETURNING *,
-(select u.name from users u where user_id = users.id) As user_name,
-(select f.name from feeds f where feed_id = feeds.id) As feed_name;
+-- name: GetFeedById :one
+Select * from
+feeds
+where  id = $1;
+
+-- name: GetFeedByUrl :one
+Select * from
+feeds
+where  url = $1;
 
 -- name: GetFeeds :many
 Select * from feeds;
