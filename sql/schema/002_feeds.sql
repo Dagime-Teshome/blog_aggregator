c399934ca7197734d54512afbea9a5c6171bfc64
Dagime Teshome <dagimeteshome1@gmail.com>
2026-01-16 19:42:12 +0300

    add feeds schema

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -9,16 +9,7 @@ Create table feeds (
     FOREIGN key (user_id) References Users (id) ON DELETE CASCADE
 );
 
-Create table feed_follows (
-    id UUID primary key,
-    created_at timestamp not null,
-    updated_at timestamp not null,
-    user_id UUID not null,
-    feed_id UUID not null,
-    FOREIGN key (user_id) References users (id) ON DELETE CASCADE,
-    FOREIGN key (feed_id) References feeds (id) ON DELETE CASCADE,
-    unique(user_id,feed_id)
-)
+
 
 -- +goose Down
 Drop table feeds;
