fb2c0abf47ce6054c4d490b445be12d6a1f890cf
Dagime Teshome <dagimeteshome1@gmail.com>
2026-01-17 18:58:39 +0300

    add logged in middleware
    -passes the current user
    -helps keep code dry

---
+++ internal/middleware/middleware_logged_in.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"go/gator/internal/cli"
+	"go/gator/internal/database"
+)
+
+func LoggedInMiddleWare(handler func(s *cli.State, cmd cli.Command, user database.User) error) func(s *cli.State, cmd cli.Command) error {
+
+	return func(s *cli.State, cmd cli.Command) error {
+		user, err := s.Db.GetUser(context.Background(), s.Config.Current_user_name)
+		if err != nil {
+			return fmt.Errorf("error fetching user %w", err)
+		}
+		handler(s, cmd, user)
+		return nil
+	}
+
+}
