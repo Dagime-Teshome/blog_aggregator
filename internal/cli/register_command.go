a80698668c8b2bbc554a52deb025089d25966b96
Dagime Teshome <dagimeteshome1@gmail.com>
2026-01-20 15:53:11 +0300

    remove unnecessary comments

--- internal/cli/register_command.go
+++ internal/cli/register_command.go
@@ -9,15 +9,6 @@ import (
 	"github.com/google/uuid"
 )
 
-// Ensure that a name was passed in the args.
-// Create a new user in the database. It should have access to the CreateUser query through the state -> db struct.
-// Pass context.Background() to the query to create an empty Context argument.
-// Use the uuid.New() function to generate a new UUID for the user.
-// created_at and updated_at should be the current time.
-// Use the provided name.
-// Exit with code 1 if a user with that name already exists.
-// Set the current user in the config to the given name.
-// Print a message that the user was created, and log the user's data to the console for your own debugging.
 func Register(s *State, cmd Command) error {
 	if len(cmd.Args) < 1 {
 		return fmt.Errorf("Arguments fewer than expected")
