5b6a1616347fd317ef3f1fc7467b10d765056222
Dagime Teshome <dagimeteshome1@gmail.com>
2026-01-22 18:36:02 +0300

    add agg command
    - add aggregate command that with duration argument given

--- internal/cli/agg_command.go
+++ internal/cli/agg_command.go
@@ -2,17 +2,95 @@ package cli
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"go/gator/internal/database"
 	feed "go/gator/internal/rss_feed"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 func Agg(s *State, cmd Command) error {
-	ctx := context.Background()
-	rssUrl := "https://www.wagslane.dev/index.xml"
-	rssfeed, err := feed.FetchFeed(ctx, rssUrl)
-	if err != nil {
-		return err
+	if len(cmd.Args) < 1 {
+		return fmt.Errorf("not enough arguments")
 	}
-	fmt.Println(rssfeed)
-	return nil
+	time_between_reqs, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("error parsing request interval: %w", err)
+	}
+
+	ticker := time.NewTicker(time_between_reqs)
+	fmt.Printf("Collecting feeds every %v\n", time_between_reqs)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *State) {
+	ctx := context.Background()
+	fetch_feed, err := s.Db.GetNextFeedToFetch(ctx)
+	if err != nil {
+		log.Println("error getting next feed %w", err)
+		return
+	}
+	log.Println("Found a feed to fetch!")
+	scrapeFeed(s.Db, fetch_feed)
+}
+
+func scrapeFeed(db *database.Queries, db_feed database.Feed) {
+	_, err := db.MarkFeedFetched(context.Background(), db_feed.ID)
+	if err != nil {
+		log.Printf("Couldn't mark feed %s fetched: %v", db_feed.Name, err)
+		return
+	}
+
+	feedData, err := feed.FetchFeed(context.Background(), db_feed.Url)
+	if err != nil {
+		log.Printf("Couldn't collect feed %s: %v", db_feed.Name, err)
+		return
+	}
+
+	for _, item := range feedData.Channel.Item {
+		fmt.Printf("Found post: %s\n", item.Title)
+		publishedAt := sql.NullTime{}
+		if t, err := time.Parse(time.RFC1123Z, item.PubDate); err == nil {
+			publishedAt = sql.NullTime{
+				Time:  t,
+				Valid: true,
+			}
+		}
+		createPost := database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now(),
+			UpdatedAt:   time.Now(),
+			FeedID:      db_feed.ID,
+			PublishedAt: publishedAt,
+			Title:       item.Title,
+			Description: sql.NullString{
+				String: item.Description,
+				Valid:  true,
+			},
+			Url: item.Link,
+		}
+		post, err := db.CreatePost(context.Background(), createPost)
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
+				continue
+			}
+			log.Printf("error creating post %v", post)
+			continue
+		}
+		fmt.Println(post)
+	}
+	log.Printf("Feed %s collected, %v posts found", db_feed.Name, len(feedData.Channel.Item))
+}
+
+func printFeed(feed feed.RSSFeed) {
+	fmt.Println(fmt.Sprintf("--------------------feeds list for %s-------------------", feed.Channel.Title))
+	for _, feed := range feed.Channel.Item {
+		fmt.Printf("feed title : %s \n", feed.Title)
+	}
 }
