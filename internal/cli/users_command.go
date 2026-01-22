8fd38892626a2ba69f4263b02ca63b98a77e629e
Dagime Teshome <dagimeteshome1@gmail.com>
2026-01-22 18:40:39 +0300

    modify users command
    -modify users command to say there are no users when there are none

--- internal/cli/users_command.go
+++ internal/cli/users_command.go
@@ -11,6 +11,10 @@ func GetUsers(s *State, cmd Command) error {
 	if err != nil {
 		return fmt.Errorf("Couldn't fetch user %w", err)
 	}
+	if len(usersList) == 0 {
+		fmt.Println("no users in database")
+		return nil
+	}
 	for _, value := range usersList {
 		if value.Name == s.Config.Current_user_name {
 			fmt.Printf("* %s (current)\n", value.Name)
