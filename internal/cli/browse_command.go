380258aa2e115960b29e41aa0137405c58fbb295
Dagime Teshome <dagimeteshome1@gmail.com>
2026-01-22 18:37:13 +0300

    add browse command
    -add a browse command that show a list of posts from feeds from the currently logged in user.

---
+++ internal/cli/browse_command.go
@@ -0,0 +1,34 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+	"go/gator/internal/database"
+	"strconv"
+)
+
+func Browse(s *State, cmd Command, user database.User) error {
+	queryLimit := int32(2)
+	if len(cmd.Args) > 0 {
+		if n, err := strconv.Atoi(cmd.Args[0]); err != nil || n <= 0 {
+			return fmt.Errorf("limit must be a positive number")
+		} else {
+			queryLimit = int32(n)
+		}
+	}
+	getParams := database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit:  queryLimit,
+	}
+	postList, err := s.Db.GetPostsForUser(context.Background(), getParams)
+	if err != nil {
+		return fmt.Errorf("error getting posts for user %s err:%v", user.Name, err)
+	}
+	if len(postList) == 0 {
+		fmt.Println("no posts in list")
+	}
+	for i, posts := range postList {
+		fmt.Printf("%v : %s \n", i, posts.Title)
+	}
+	return nil
+}
