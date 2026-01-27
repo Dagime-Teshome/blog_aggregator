809398af64a0afa1e98ad67a4b66f5dd990cd49f
Dagime Teshome <dagimeteshome1@gmail.com>
2026-01-27 16:43:26 +0300

    change variable names and error texts

--- internal/cli/login_command.go
+++ internal/cli/login_command.go
@@ -7,15 +7,14 @@ import (
 
 func HandlerLogin(s *State, cmd Command) error {
 	if len(cmd.Args) == 0 {
-		return fmt.Errorf("User name Required")
+		return fmt.Errorf("username required")
 	}
 	ctx := context.Background()
-	feted_user, err := s.Db.GetUser(ctx, cmd.Args[0])
+	fetchedUser, err := s.Db.GetUser(ctx, cmd.Args[0])
 	if err != nil {
-		return fmt.Errorf("User doesn't exist %v", err)
+		return fmt.Errorf("user doesn't exist: %w", err)
 	}
-	err = s.Config.SetUser(feted_user.Name)
-	if err != nil {
+	if err := s.Config.SetUser(fetchedUser.Name); err != nil {
 		return err
 	}
 	fmt.Printf("user %s has been set\n", cmd.Args[0])
